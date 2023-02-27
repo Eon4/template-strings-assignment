6df6ceb19057390105ce21bc6f84fafa14bdb4a5
Eon4 <ladyforest85@gmail.com>
2023-02-27 13:53:57 +0100

    update 2 and 3
    Assignment 2 and 3 are working

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -6,16 +6,41 @@ const navn = "Keld";
         const aarIvirksomhed = 3;
         const besked = `Hej ${navn}
         du har arbejdet i vores virksomhed i ${aarIvirksomhed} år.`;
-        console.log(besked);
+        // console.log(besked);
 
 document.getElementById("message").innerHTML = besked;
 
 
 /* Opgave 2*/
 
+const myData=[
+    { "name": "T-shirt",
+    "description": "En t-shirt lavet af bomuld",
+    "price": 99 },
+    { "name": "Jeans",
+    "description": "Blå denim jeans med straight fit",
+    "price": 299
+    },
+    { "name": "Hættetrøje",
+    "description": "En grå hættetrøje med lynlås",
+    "price": 199 }
+    ];
 
-// din kode her
+    // console.log(myData);
 
+    const productList = document.getElementById("produktListe");
+    // console.log(produktListe);
+    
+    myData.map((dataElement) => {
+      let article = document.createElement("article");
+      let myHTML = `<article class="produkt">
+    <h2>${dataElement.name}</h2>
+    <p>${dataElement.description}</p>
+    <p>Pris: ${dataElement.price} kr.</p>
+    </article>`;
+      article.innerHTML = myHTML;
+      productList.append(article);
+    });
 
 
 /* Opgave 3 - skriv videre på koden her: */
@@ -30,3 +55,35 @@ const submitButton = document.getElementById('indsend-knap');
 const personList = document.getElementById('person-liste');
 
 
+// submit button event listner med arrow function
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const myFormFelter = myForm.querySelectorAll('input');
+
+    /* push resultatet fra formen ind på myPersons.
+    vi ved der er 2 elementer derfor direkte adressering ved hjælp af []
+    */
+    myPersons.push({
+    navn: myFormFelter[0].value,
+    alder: myFormFelter[1].value,
+    });
+    
+    // Sorter listen efter alder og vis den på siden
+    const sorteretMyPersons = sorterEfterAlder(myPersons);
+    showList(sorteretMyPersons);
+    });
+
+    // view code
+    function showList(sorteretMyPersons) {
+    let html = '';
+    sorteretMyPersons.map((person) => {
+    // html template.
+    html += `<li>${person.navn} er ${person.alder} år gammel.</li>`;
+    });
+    personList.innerHTML = html;
+    }
+
+    // Funktion til at sortere listen efter alder
+    function sorterEfterAlder(myPersons) {
+    return myPersons.sort((a, b) => a.alder - b.alder);
+    }
